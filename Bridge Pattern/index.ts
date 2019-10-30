fc5eda6caa261a1d3c2b7a986caa9b65f4c2188e
jeontaeyun <jow1813@naver.com>
2019-10-30 09:53:20 +0900

    Feature : Bridge Pattern renew

--- Bridge Pattern/index.ts
+++ Bridge Pattern/index.ts
@@ -1,32 +1,77 @@
-class MorseCode {
-	public dot(): void {
-		console.log('');
+/**
+ * 브리지 패턴이 적용
+ * 기능이 다양화 될 때(만약 모스부호를 소리로 만들거나 빛으로 만들 때)
+ * 유지 보수 및 확장에 용이해진다.
+ */
+
+//MorseCode안의 기능의 구조화
+interface MorseCodeFunction {
+	dot(): void;
+	dash(): void;
+	space(): void;
+}
+
+class DefaultFunction implements MorseCodeFunction {
+	public constructor() {
+		return;
 	}
-	public dash(): void {
-		console.log('');
+	public dot() {
+		process.stdout.write('.');
 	}
-	public space(): void {
-		console.log('');
-		s;
+	public dash() {
+		process.stdout.write('-');
+	}
+	public space() {
+		process.stdout.write(' ');
 	}
 }
 
-class PrintMorse extends MorseCode {
-	public g(): void {
-		this.dash();
-		this.dash();
-		this.dot();
-		this.space();
+class SoundFunction implements MorseCodeFunction {
+	public dot(): void {
+		process.stdout.write('음');
 	}
-	public a(): void {
-		this.dot();
-		this.dash();
-		this.space();
+	public dash(): void {
+		process.stdout.write('파');
 	}
-	public r(): void {
-		this.dot();
-		this.dash();
-		this.dot();
-		this.space();
+	public space(): void {
+		process.stdout.write('삐~');
 	}
 }
+
+class PrintMorse {
+	private morseFunction;
+	public constructor(func: MorseCodeFunction) {
+		this.morseFunction = func;
+	}
+	public setFunction(func: MorseCodeFunction) {
+		this.morseFunction = func;
+	}
+	public g(): PrintMorse {
+		this.morseFunction.dash();
+		this.morseFunction.dash();
+		this.morseFunction.dot();
+		this.morseFunction.space();
+		return this;
+	}
+	public a(): PrintMorse {
+		this.morseFunction.dot();
+		this.morseFunction.dash();
+		this.morseFunction.space();
+		return this;
+	}
+	public r(): PrintMorse {
+		this.morseFunction.dot();
+		this.morseFunction.dash();
+		this.morseFunction.dot();
+		this.morseFunction.space();
+		return this;
+	}
+}
+
+const printMorse = new PrintMorse(new SoundFunction());
+
+printMorse.g().r();
+
+printMorse.setFunction(new DefaultFunction());
+
+printMorse.g().r();
